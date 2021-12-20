7423d0868cc298e3fec0264cde394deb828aab03
jcollier <jcollier@scottlogic.com>
2021-12-20 17:03:26 +0000

    added user post service

---
+++ src/main/java/com/scottlogic/UserPostService.java
@@ -0,0 +1,37 @@
+package com.scottlogic;
+
+import com.scottlogic.filters.combine.AndCombineFilters;
+import com.scottlogic.filters.combine.OrCombineFilters;
+
+import java.util.List;
+
+public class UserPostService {
+    private static List<UserPost> userPostList;
+
+    public UserPostService(List<UserPost> userPostList) {
+        UserPostService.userPostList = userPostList;
+    }
+
+    public static List<UserPost> postSort(PostSorter postSorter, SortOrder sortOrder) {
+        return postSorter.sort(userPostList, sortOrder);
+    }
+
+    public static List<UserPost> postFilter(PostFilter postFilter) {
+        return postFilter.filter(userPostList);
+    }
+
+    public static List<UserPost> filterSorter(PostFilter postFilter, PostSorter postSorter, SortOrder sortOrder) {
+        List<UserPost> outputList = postFilter.filter(userPostList);
+        return postSorter.sort(outputList, sortOrder);
+    }
+
+    public List<UserPost> andFilter(PostFilter filter1, PostFilter filter2) {
+        final AndCombineFilters andCombineFilters = new AndCombineFilters(filter1, filter2);
+        return andCombineFilters.filter(userPostList);
+    }
+
+    public List<UserPost> orFilter(PostFilter filter1, PostFilter filter2) {
+        final OrCombineFilters orCombineFilters = new OrCombineFilters(filter1, filter2);
+        return orCombineFilters.filter(userPostList);
+    }
+}
